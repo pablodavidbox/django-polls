4261b7d29a7194d566750d32f313fca6a0c064a2
Pablodavid <harlockraimar@gmail.com>
2025-05-09 16:59:03 -0300

    Importar Question en views.py

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-
+from .models import Question
 # Create your views here.
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("This is the index view.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def owner(request):
     return HttpResponse("Hello, world. 071b5ec6 is the index of the polls.")
