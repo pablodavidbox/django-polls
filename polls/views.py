4eb9ede14e7caf91427958a4769d420f5ee2955d
Pablodavid <harlockraimar@gmail.com>
2025-05-09 16:27:36 -0300

    Parte 3 - Vistas y plantillas

--- polls/views.py
+++ polls/views.py
@@ -4,4 +4,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hola, esta es la app de encuestas.")
+    return HttpResponse("This is the index view.")
+
+def owner(request):
+    return HttpResponse("Hello, world. 071b5ec6 is the index of the polls.")
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}.")
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
